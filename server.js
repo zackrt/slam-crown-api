81ef42d02f01b75ff1af34e9b28089fff670febf
Zack Toth <zacharyrtoth@gmail.com>
2018-04-17 20:29:18 -0700

    installed cors

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const cors = require('cors');
+const {CLIENT_ORIGIN} = require('./config');
+
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+);
 
 app.get('/api/*', (req, res) => {
   res.json({ok: true});
