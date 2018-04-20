7bb2e5927acbe6787f2bd5faddccff75881d10ba
Zack Toth <zacharyrtoth@gmail.com>
2018-04-19 21:29:44 -0700

    changed exports to configjs

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
-const URL = DATABASE_URL;
-
-DATABASE_URL = mongodb://<dbuser>:<dbpassword>@ds015325.mlab.com:15325/zackrt-mongodb-mlab;
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://slamcrownadmin:2018reactor@ds015325.mlab.com:15325/zackrt-mongodb-mlab';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/users';
+exports.PORT = process.env.PORT || 8080;
+exports.JWT_SECRET = process.env.JWT_SECRET || 'secret';
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
