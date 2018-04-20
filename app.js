3a9b11bf6ca570d5bbe48e0210b4b09fa875d9f2
Zack Toth <zacharyrtoth@gmail.com>
2018-04-19 20:12:25 -0700

    began adding restful api endpoints

--- app.js
+++ app.js
@@ -15,6 +15,15 @@ app.get('/api/*', (req, res) => {
   res.json({ok: true});
 });
 
+app.post('/api/:id', (req,res) => {
+  res.json({ok:true})
+    .then()
+    .catch(err);
+});
+app.put('/api/:id', (req.,res) => {
+
+});
+
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 module.exports = app;
---
+++ config.js
@@ -0,0 +1,3 @@
+const URL = DATABASE_URL;
+
+DATABASE_URL = mongodb://<dbuser>:<dbpassword>@ds015325.mlab.com:15325/zackrt-mongodb-mlab;
