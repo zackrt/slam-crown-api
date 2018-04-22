9854dbe2b911f987e8adfcfda5e8f0f854983df0
Zack Toth <zacharyrtoth@gmail.com>
2018-04-22 09:10:43 -0700

    added post router in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+/*local strategies needed*/
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.post('/login)
 
 module.exports = router;
