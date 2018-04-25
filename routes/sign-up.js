6196a5245d7a8d86458fa2fab2826985378e6066
Zack Toth <zacharyrtoth@gmail.com>
2018-04-24 22:55:11 -0700

    added router.post to sign up

--- routes/sign-up.js
+++ routes/sign-up.js
@@ -5,5 +5,18 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('Slam Crown Sign-up page');
 });
+/* 
+needs router.post for EmailAddress, hashed password, and date of concussion 
+*/
+router.post('/', jwtAuth, function (req, res) {
+  let RequiredFields = {EmailAddress, password}
+  try {
+    SlamCrownUser.create({EmailAddress: req.body.EmailAddress}).then(user => {
+        res.status(201).json({user:user.serialize()})
+  });
+     } catch(err) {
+            res.json({err})
+     };
+});
 
 module.exports = router;
