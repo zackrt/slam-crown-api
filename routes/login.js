9e075bacd96409452f82a9d6237edd4bbce13c6f
Zack Toth <zacharyrtoth@gmail.com>
2018-04-25 21:42:56 -0700

    added passport-local

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const { SlamCrownUser } = require('../models/SlamCrownUsers');
 
 const passport = require('passport');
-const jwtAuth = passport.authenticate('jwt', { session: false });
+
 
 router.get('/', function(req, res, next) {
   res.render('login', { title: 'Slam Crown Login' });
--- routes/sign-up.js
+++ routes/sign-up.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var bodyparser = require 
 const { SlamCrownUser } = require('../models/SlamCrownUsers');
-/* GET users listing. */
+/* GET users listing. to render the page */
 router.get('/', function(req, res, next) {
   res.send('Slam Crown Sign-up page');
 });
 /* 
 needs router.post for EmailAddress, hashed password, and date of concussion 
-*/
-router.post('/', jwtAuth, function (req, res) {
+ create a new user*/
+router.post('/', bodyparser, function (req, res) {
   let RequiredFields = {EmailAddress, password}
   try {
     SlamCrownUser.create({EmailAddress: req.body.EmailAddress}).then(user => {
