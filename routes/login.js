601f2157f53842920aaf4358440637b388b1e09c
Zack Toth <zacharyrtoth@gmail.com>
2018-04-25 19:38:14 -0700

    edited login endpoints

--- routes/login.js
+++ routes/login.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var router = express.Router();
 const { SlamCrownUser } = require('../models/SlamCrownUsers');
+
 const passport = require('passport');
 const jwtAuth = passport.authenticate('jwt', { session: false });
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Slam Crown Login' });
+  res.render('login', { title: 'Slam Crown Login' });
 });
 router.post('/', function(res, req) {
   let {password, EmailAddress} = req.body;
   res.body
 });
-
 
 module.exports = router;
--- routes/sign-up.js
+++ routes/sign-up.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+const { SlamCrownUser } = require('../models/SlamCrownUsers');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('Slam Crown Sign-up page');
@@ -8,6 +8,9 @@ router.get('/', function(req, res, next) {
 /* 
 needs router.post for EmailAddress, hashed password, and date of concussion 
 */
+router.get ('/', function (req, res) {
+    SlamCrownUser
+});
 router.post('/', jwtAuth, function (req, res) {
   let RequiredFields = {EmailAddress, password}
   try {
