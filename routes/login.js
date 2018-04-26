12f644a6270884b5c2fa292c08f4f2bb5709730f
Zack Toth <zacharyrtoth@gmail.com>
2018-04-25 22:40:05 -0700

    fixed router.post

--- routes/login.js
+++ routes/login.js
@@ -5,13 +5,23 @@ const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 const passport = require('passport');
 
-
-router.get('/', bodyParser, function(req, res, next) {
-  res.render('login', { title: 'Slam Crown Login' });
+router.get('/', jsonParser, function(req, res, next) {
+  res.render('login', { title: 'Slam Crown Login' })
+    .status(200);
 });
-router.post('/', bodyParser, function(res, req) {
+router.post('/', jsonParser, (req, res) => {
   let {password, EmailAddress} = req.body;
-  res.body
+  const requiredFields = [ 'password', 'EmailAddress' ];
+  const missingField = requiredFields.find(field => !(field in req.body));
+  if (missingField) {
+    console.log('missing entity field');
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Missing field',
+      location: missingField
+    });
+  }
 });
 
 module.exports = router;
