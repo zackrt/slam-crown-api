082503d365777e41cc3d77dc5ec732b9e35b9396
Zack Toth <zacharyrtoth@gmail.com>
2018-04-27 19:31:38 -0700

    added userreporthistory.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var localStrategy = require('/auth/strategies');
 /*local strategies needed*/
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', localStrategy, function(req, res, next) {
   res.render('index', { title: 'Slam Crown App' });
 });
 
