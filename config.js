299e1ef4eb6aa768ee8192a175ba3991bd056542
Zack Toth <zacharyrtoth@gmail.com>
2018-05-03 19:14:41 -0700

    add API base url

--- config.js
+++ config.js
@@ -3,3 +3,9 @@ exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhos
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'secret';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
+module.exports = {
+    PORT: process.env.PORT || 8080,
+    // other stuff
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
+      "http://localhost:3000/api"
+  };
