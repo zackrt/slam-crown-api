29631441fc82fa8ccfc7efa0319c95fae65742ea
Zack Toth <zacharyrtoth@gmail.com>
2018-05-05 12:27:16 -0700

    edited server.js

--- server.js
+++ server.js
@@ -27,7 +27,6 @@ function runServer() {
     });
   });
 }
-
 // both runServer and closeServer need to access the same
 // server object, so we declare `server` here, and then when
 // runServer runs, it assigns a value.
